5be6c5169cf74345170861a7513757ca3cad5f2d
ADMIN <ADMIN@PC228>
2024-06-27 09:23:23 +0700

    Add project files.

---
+++ ExamWeb_NguyenTanPhong/Migrations/20240627011149_KhoiTao.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExamWeb_NguyenTanPhong.Migrations
+{
+    public partial class KhoiTao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TuaDe = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    DienVien = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TrongNuoc = table.Column<bool>(type: "bit", nullable: false),
+                    GiaVe = table.Column<double>(type: "float", nullable: false),
+                    ThoiLuong = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ ExamWeb_NguyenTanPhong/Models/Phim.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamWeb_NguyenTanPhong.Models
+{
+    public class Phim
+    {
+        public int Id { get; set; }
+        [StringLength(250)]
+        [Required]
+        public string TuaDe { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string DienVien { get; set; }
+
+        public bool TrongNuoc { get; set; }
+        public double GiaVe { get; set; }
+        public int ThoiLuong { get; set; }
+    }
+}
